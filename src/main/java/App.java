d85fe45ddda4ffa415e738f0cef6149bc8a9098d
Stephen Rooney <s.f.rooney@gmail.com>
2018-09-18 19:09:31 +0100

    working on structure

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,8 @@
 public class App {
+
+    public static void main(String[] args) {
+        //TODO: implement detecting changes to input file
+        //Parse Input file and create java objects relative to xml tags
+
+    }
 }
